f5fbfc5fb07aa285630e0ddb21bfa5eb7e2fdb84
TianShuang Ke <qinyun575@gmail.com>
2024-12-11 10:19:16 +0800

    [test]Add test

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
+git clone https://github.com/Microsoft/vcpkg.git
+cd vcpkg
+git checkout tags/2020.04
+./bootstrap-vcpkg.sh
 
-export VCPKG_ROOT=/home/listenai/Applications/vcpkg
+export VCPKG_ROOT=/home/listenai/App/vcpkg
+export PATH=${VCPKG_ROOT}:$PATH
+
+vcpkg install spdlog fmt Catch2 asio
 
 export spdlog_DIR=${VCPKG_ROOT}/installed/x64-linux/share/spdlog
 export fmt_DIR=${VCPKG_ROOT}/installed/x64-linux/share/fmt/
 export Catch2_DIR=${VCPKG_ROOT}/installed/x64-linux/share/catch2
 export asio_DIR=${VCPKG_ROOT}/installed/x64-linux/share/asio
+# export PATH=${VCPKG_ROOT}/installed/x64-linux/bin:$PATH
 
 
 cmake -G Ninja -B build && cmake --build build --config Release
@@ -14,7 +22,7 @@ wget https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q1-update/+download/gcc
 sudo tar -xvf gcc-arm-none-eabi-7-2018-q2-update-linux.tar.bz2 -C /usr/local/
 sudo tar -xvf gcc-arm-none-eabi-4_9-2015q1-20150306-linux.tar.bz2 -C /usr/local/
 
-export TMP=/media/listenai/TsMax/Bluetooth/Tools_src/temp
+export TMP=/home/listenai/Develop/Bluetooth/Tools_src/temp
 mkdir ${TMP}
 export GNUARMEMB_TOOLCHAIN_PATH=/opt/gcc-arm-none-eabi-10.3-2021.10/bin
 cmake -G Ninja -B build -DCOMPILE_CONNECTIVITY=1 -DCONNECTIVITY_VERSION=1.0.0
